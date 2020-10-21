9b08b4db7e30e9f8ca27578d2d7b0e342cb3ddc3
ErickaV17 <71712202+ErickaV17@users.noreply.github.com>
2020-10-21 14:09:58 -0400

    worked on arraystuff

--- ArrayStuff.java
+++ ArrayStuff.java
@@ -1,7 +1,7 @@
 /*
- * Programmer:
- * Date:
- * Purpose:
+ * Programmer:Ericka Van Dongen
+ * Date: October 21, 2020
+ * Purpose: complete the TODO comments
  */
 
 import java.util.Scanner;
@@ -13,32 +13,37 @@ public class ArrayStuff {
 
         // PART A
 
-        // TODO: Create integer array called "ages" with length 5.
+        // Create integer array called "ages" with length 5.
+        int [] ages = new int [5];
 
+        // Change the 3rd element of "ages" to be 18
+        ages[3] = 18;
 
-        // TODO: Change the 3rd element of "ages" to be 18
-
-
-        // TODO: Change the first element of "ages" to be an integer entered by the user
+        // Change the first element of "ages" to be an integer entered by the user
         System.err.println("Enter an integer:");
+        ages[0] = input.nextInt();
 
+        // Change the last element of "ages" to be the first element squared
+        ages[17] = 0 * 2;
 
-        // TODO: Change the last element of "ages" to be the first element squared
-
-
-        // TODO: Print all of the elements of "ages", one per line
-
+        // Print all of the elements of "ages", one per line
+        System.out.println(ages[0]);
+        System.out.println(ages[1]);
+        System.out.println(ages[2]);
+        System.out.println(ages[3]);
+        System.out.println(ages[4]);
+        System.out.println(ages[5]);
 
 
         // PART B
 
-        // TODO: Create AND initialize an array called "planets" that holds the
+        // Create AND initialize an array called "planets" that holds the
         // names of the planets in order from closest to the sun to farthest.
         // NOTE: Pluto is NOT a planet.
+        String [] planets = {"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"};
 
-
-        // TODO: Print the first letter of the 7th planet from the sun
-
+        // Print the first letter of the 7th planet from the sun
+        System.out.println(planets[6]);
 
         // TODO: Change the name of our planet to be a line of text that the user inputs
         System.err.println("What do you want to rename Earth to be?");
